c484dafda8828b163edd9fa66ed6a4d8e384f1a3
henny <kimhyein1105@gmail.com>
2024-01-24 22:50:12 +0900

    Routes로 페이지 나누기

--- src/Main.js
+++ src/Main.js
@@ -1,12 +1,24 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './components/App';
 import HomePage from './pages/HomePage';
+import CoursePage from './pages/CoursePage';
+import CourseListPage from './pages/CourseListPage';
+import WishlistPage from './pages/WishlistPage';
 
 function Main() {
-  return (
-    <App>
-      <HomePage />
-    </App>
-  );
+	return (
+		<BrowserRouter>
+			<App>
+				<Routes>
+					<Route pate='/' element={<HomePage />} />
+					{/* / 링크가 들어오면 HomePage 컴포넌트를 보여주기 jsx를 넘겨주는 것 */}
+					<Route pate='courses' element={<CourseListPage />} />
+					<Route pate='courses/react-fronted-development' element={<CoursePage />} />
+					<Route path='wishlist' element={<WishlistPage />} />
+				</Routes>
+			</App>
+		</BrowserRouter>
+	);
 }
 
 export default Main;
