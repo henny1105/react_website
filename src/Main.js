2993273a737e16b659480f35f943f52c96ba46c5
henny <kimhyein1105@gmail.com>
2024-01-25 21:25:39 +0900

    하위 페이지 나누기

--- src/Main.js
+++ src/Main.js
@@ -10,17 +10,16 @@ import QuestionListPage from './pages/QuestionListPage';
 function Main() {
 	return (
 		<BrowserRouter>
-			<App>
-				<Routes>
-					<Route path='/' element={<HomePage />} />
-					{/* / 링크가 들어오면 HomePage 컴포넌트를 보여주기 jsx를 넘겨주는 것 */}
-					<Route path='courses' element={<CourseListPage />} />
-					<Route path='courses/react-fronted-development' element={<CoursePage />} />
-					<Route path='questions' element={<QuestionListPage />} />
-					<Route path='questions/616825' element={<QuestionPage />} />
-					<Route path='wishlist' element={<WishlistPage />} />
-				</Routes>
-			</App>
+			<Routes path='/' element={<App />}>
+				<Route index element={<HomePage />} />
+				<Route path='courses'>
+					<Route index element={<CourseListPage />} />
+					<Route path='react-fronted-development' element={<CoursePage />} />
+				</Route>
+				<Route path='questions' element={<QuestionListPage />} />
+				<Route path='questions/616825' element={<QuestionPage />} />
+				<Route path='wishlist' element={<WishlistPage />} />
+			</Routes>
 		</BrowserRouter>
 	);
 }
