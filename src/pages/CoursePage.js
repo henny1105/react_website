ebe77d91b4469d3bd8557d921301c10f47155124
henny <kimhyein1105@gmail.com>
2024-01-25 21:32:35 +0900

    useParams로 동적인 경로 만들기

--- src/pages/CoursePage.js
+++ src/pages/CoursePage.js
@@ -1,3 +1,4 @@
+import { useParams } from 'react-router-dom';
 import { addWishlist, getCourseBySlug } from '../api';
 import Button from '../components/Button';
 import Container from '../components/Container';
@@ -7,39 +8,40 @@ import getCourseColor from '../utils/getCourseColor';
 import styles from './CoursePage.module.css';
 
 function CoursePage() {
-  const course = getCourseBySlug('react-frontend-development');
-  const courseColor = getCourseColor(course?.code);
+	const { courseSlug } = useParams();
+	const course = getCourseBySlug(courseSlug);
+	const courseColor = getCourseColor(course?.code);
 
-  const headerStyle = {
-    borderTopColor: courseColor,
-  };
+	const headerStyle = {
+		borderTopColor: courseColor,
+	};
 
-  const handleAddWishlistClick = () => {
-    addWishlist(course?.slug);
-  };
+	const handleAddWishlistClick = () => {
+		addWishlist(course?.slug);
+	};
 
-  return (
-    <>
-      <div className={styles.header} style={headerStyle}>
-        <Container className={styles.content}>
-          <CourseIcon photoUrl={course.photoUrl} />
-          <h1 className={styles.title}>{course.title}</h1>
-          <Button variant="round" onClick={handleAddWishlistClick}>
-            + 코스 담기
-          </Button>
-          <p className={styles.summary}>{course.summary}</p>
-        </Container>
-      </div>
-      <Container className={styles.topics}>
-        {course.topics.map(({ topic }) => (
-          <Card className={styles.topic} key={topic.slug}>
-            <h3 className={styles.title}>{topic.title}</h3>
-            <p className={styles.summary}>{topic.summary}</p>
-          </Card>
-        ))}
-      </Container>
-    </>
-  );
+	return (
+		<>
+			<div className={styles.header} style={headerStyle}>
+				<Container className={styles.content}>
+					<CourseIcon photoUrl={course.photoUrl} />
+					<h1 className={styles.title}>{course.title}</h1>
+					<Button variant='round' onClick={handleAddWishlistClick}>
+						+ 코스 담기
+					</Button>
+					<p className={styles.summary}>{course.summary}</p>
+				</Container>
+			</div>
+			<Container className={styles.topics}>
+				{course.topics.map(({ topic }) => (
+					<Card className={styles.topic} key={topic.slug}>
+						<h3 className={styles.title}>{topic.title}</h3>
+						<p className={styles.summary}>{topic.summary}</p>
+					</Card>
+				))}
+			</Container>
+		</>
+	);
 }
 
 export default CoursePage;
