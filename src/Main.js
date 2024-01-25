e747d738197f3fdf565bdfea8fd715f8e52ae790
henny <kimhyein1105@gmail.com>
2024-01-25 21:15:59 +0900

    커뮤니티 페이지 추가하기

--- src/Main.js
+++ src/Main.js
@@ -4,6 +4,8 @@ import HomePage from './pages/HomePage';
 import CoursePage from './pages/CoursePage';
 import CourseListPage from './pages/CourseListPage';
 import WishlistPage from './pages/WishlistPage';
+import QuestionPage from './pages/QuestionPage';
+import QuestionListPage from './pages/QuestionListPage';
 
 function Main() {
 	return (
@@ -14,6 +16,8 @@ function Main() {
 					{/* / 링크가 들어오면 HomePage 컴포넌트를 보여주기 jsx를 넘겨주는 것 */}
 					<Route path='courses' element={<CourseListPage />} />
 					<Route path='courses/react-fronted-development' element={<CoursePage />} />
+					<Route path='questions' element={<QuestionListPage />} />
+					<Route path='questions/616825' element={<QuestionPage />} />
 					<Route path='wishlist' element={<WishlistPage />} />
 				</Routes>
 			</App>
